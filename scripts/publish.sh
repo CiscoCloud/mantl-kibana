9d8f45262df925ad995e9410f39fffc119d73167
ryane <ryanesc@gmail.com>
2016-03-28 15:09:13 -0400

    publish latest tag on tagged release

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,3 +6,7 @@ version=${TRAVIS_TAG:-edge}
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t $repo/mantl-kibana:$version --rm .
 docker push $repo/mantl-kibana:$version
+
+if [ "$version" != 'edge' ]; then
+    docker push $repo/mantl-kibana:latest
+fi
