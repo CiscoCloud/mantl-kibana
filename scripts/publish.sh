565a04ecad934ec16ef6e0c16e23f7f13cdf258f
ryane <ryanesc@gmail.com>
2016-03-28 15:35:18 -0400

    fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,5 +8,6 @@ docker build -t $repo/mantl-kibana:$version --rm .
 docker push $repo/mantl-kibana:$version
 
 if [ "$version" != 'edge' ]; then
+    docker tag $repo/mantl-kibana:$version $repo/mantl-kibana:latest
     docker push $repo/mantl-kibana:latest
 fi
